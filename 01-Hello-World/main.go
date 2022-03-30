8f7c47cbe461afaba8f5c563b8fd0398beea3b8d
maevadevs <maevadevs@users.noreply.github.com>
2022-03-30 01:56:06 -0500

    Finish review

--- 01-Hello-World/main.go
+++ 01-Hello-World/main.go
@@ -1,22 +1,22 @@
 // Package
 // *******
 // Package == Project == Workspace
-// - A Package can have many files associated with it
-// - Each file ending with .go
-// - The very first line of each file in the same package must start with the package declaration
+// 	- A Package can have many files associated with it
+// 	- Each file ending with .go
+// 	- The very first line of each file in the same package must start with the package declaration
 
 // There are 2 types of packages:
 //	1. Executable
 //		- Always "main" package
 //		- Must always have a function called "main()" as well
 //		- Generates a .exe/.bin file that we can run when compiled
-//		- Code used when we want to do something (Executed codes)
+//		- Code used when we want to do something (Executable codes)
 //	2. Reusable
 //		- Any other package name other than "main"
 //		- Code used as "helpers" for reusable logic, libraries, dependencies
-// - Files in the same package do not have to be imported into each other before they can be used
-// - If other packages are declared with `package main`, the functions they contains can be used here directly
-// - But one of the file must contain the the "main()" function as the primary entry
+// 	- Files in the same package do not have to be imported into each other before they can be used
+// 	- If other packages are declared with "package main", the functions they contains can be used here directly
+// 	- But one of the file must contain the "main()" function as the primary entry
 
 package main
 
@@ -45,23 +45,20 @@ func main() {
 // How is a Go file organized?
 // ***************************
 // In practice, always the same typical pattern
-//
-// 1. Package
-// 2. Imports
-// 3. Functions
+// 	1. Package
+// 	2. Imports
+// 	3. Functions
 
-/* How do we run code in our project?
- * **********************************
- * > go run <filename>
- * > go run "<filepath>"
- */
+// How do we run code in our project?
+// **********************************
+// > go run <filename>
+// > go run "<filepath>"
 
-/* Available Go Commands
- * *********************
- * > go build 	- Compiles a bunch of go source code files into executable binaries
- * > go run 	- Compiles and execute a bunch of go source code files (build + run) but does not produce an actual executable
- * > go fmt		- Formats all the code in each file in the current directory
- * > go install	- Compiles and "install" a package
- * > go get		- Download the raw source code of someone else's package
- * > go test	- Runs any tests associated with the current projects
- */
+// Available Go Commands
+// *********************
+// > go build 	- Compiles a bunch of go source code files into executable binaries
+// > go run 	- Compiles and execute a bunch of go source code files (build + run) but does not produce an actual executable
+// > go fmt		- Formats all the code in each file in the current directory
+// > go install	- Compiles and "install" a package
+// > go get		- Download the raw source code of someone else's package
+// > go test	- Runs any tests associated with the current projects
---
+++ 03-Structs/main.go
@@ -0,0 +1,24 @@
+// Package Declaration
+// *******************
+package main
+
+// Import Statements
+// *****************
+
+// Structs
+// *******
+//	- A Data Structure in Go, used widely throughout Go
+//	- A collection of properties that are related together
+//	- Similar to Python Dictionary, but not an object
+type person struct {
+	first_name string
+	last_name  string
+}
+
+// Function Definitions
+// ********************
+func main() {
+
+	// Creating a value of type person
+
+}
